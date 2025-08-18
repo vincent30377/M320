1d5c721f5e9e64542728894715dadfc4c3d00ea1
vincent.haucke <vincent.haucke@gmail.com>
2025-08-18 12:05:53 +0200

    kgv_ggt

--- ConsolApp_001/MyMath.cs
+++ ConsolApp_001/MyMath.cs
@@ -12,5 +12,28 @@ namespace ConsolApp_001
         {
             return a + 2 * b; 
         }
+
+        static internal int Calc_ggT(int a, int b)
+        {
+            int z;
+
+            ;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+
+            return a;
+            
+        }
+        static internal int Calc_kgv(int a, int b)
+        {
+            return a * b / Calc_ggT(a, b);
+        }
+        
+
+
     }
 }
--- ConsolApp_001/Program.cs
+++ ConsolApp_001/Program.cs
@@ -6,10 +6,17 @@
         {
             Console.WriteLine("Hello, World!");
 
-            int a = 2;
-            int b = 3;
+            int a = 3;
+            int b = 5;
             int summe = MyMath.Addiere(a, b);
             Console.WriteLine($"Summe von {a} und {b} = {summe}");
+
+            int ggt = MyMath.Calc_ggT(a, b);
+            Console.WriteLine($"Der ggt von {a} und {b} ist {ggt}");
+
+            int kgv = MyMath.Calc_kgv(a, b);
+            Console.WriteLine($"Der kgv von {a} und {b} ist {kgv}");
+
         }
     }
-}
+}   
