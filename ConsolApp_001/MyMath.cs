f10ff3eabdc322c19d340629dc6ac0c7180055af
vincent.haucke <vincent.haucke@gmail.com>
2025-08-18 09:31:58 +0200

    Projektdateien hinzufügen.

---
+++ ConsolApp_001/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolApp_001
+{
+    internal class MyMath
+    {
+        static internal int Add(int a, int b)
+        {
+            return a + b; 
+        }
+    }
+}
---
+++ ConsolApp_001/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsolApp_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 2;
+            int b = 3;
+            int summe = MyMath.Add(a, b);
+            Console.WriteLine($"Summe von {a} und {b} = {summe}");
+        }
+    }
+}
