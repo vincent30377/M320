758699384c4bc9cd7a03f2549b4e8eb6ac96b59b
vincent.haucke <vincent.haucke@gmail.com>
2025-11-24 12:03:27 +0100

    s

--- WasserFass_Aufgabe/Wasserfass.cs
+++ WasserFass_Aufgabe/Wasserfass.cs
@@ -8,36 +8,58 @@ namespace WasserFass_Aufgabe
 {
     public class Wasserfass
     {
-        private readonly int minimalPegel = 0;
-        private readonly int maximalPegel;
-        public int fuellstand { get; private set; }
-        public int kapazitaet => maximalPegel;
+        private int minimalPegel;
+        private int maximalPegel;
+        private int fuellstand;
 
+        public int MinimalPegel
+        {
+            get { return minimalPegel; }
+        }
+        public int MaximalPegel
+        {
+            get { return maximalPegel; }
+        }
+        public int Fuellstand
+        {
+            get { return fuellstand; }
+        }
+        public int Kapazitaet
+        {
+            get { return maximalPegel - minimalPegel; }
+        }
+        public Wasserfass(int kapazitaet)
+        {
+            minimalPegel = 0;
+            maximalPegel = kapazitaet;
+            fuellstand = 0;
+        }
         public void Befuellen(int menge)
         {
-            if (menge < 0)
-                Console.WriteLine("menge darf nicht negativ sein");
-
-            fuellstand += menge;
-
-            if (fuellstand > maximalPegel)
-                fuellstand = maximalPegel;
+            if (menge > 0)
+            {
+                fuellstand = fuellstand + menge;
+                if (fuellstand > maximalPegel)
+                {
+                    fuellstand = maximalPegel;
+                }
+            }
         }
         public void Entnehmen(int menge)
         {
-            if (menge < 0)
-                Console.WriteLine("menge darf nicht negativ sein");
-
-            fuellstand -= menge;
-
-            if (fuellstand < minimalPegel)
-                fuellstand = minimalPegel;
+            if (menge > 0)
+            {
+                fuellstand = fuellstand - menge;
+                if (fuellstand < minimalPegel)
+                {
+                    fuellstand = minimalPegel;
+                }
+            }
         }
-
         public void Entleeren()
         {
             fuellstand = minimalPegel;
-
         }
     }
 }
+s
--- WasserFass_AufgabeTests/WasserfassTests.cs
+++ WasserFass_AufgabeTests/WasserfassTests.cs
@@ -8,18 +8,105 @@ using System.Threading.Tasks;
 
 namespace WasserFass_Aufgabe.Tests
 {
-    [TestClass()]
-    public class WasserfassTests
+    [TestClass]
+    public sealed class WasserfassTest
     {
-        [TestMethod()]
-        public void BefuellenTest()
+        [TestMethod]
+        public void Befuellen_Normal()
         {
-            fuellstand = 0;
-            Wasserfass befuellen = new Wasserfass();
-
-            int menge = Wasserfass.Befuellen(12);
-
-            Assert.AreEqual(12, menge);
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            // Act
+            fass.Befuellen(40);
+            // Assert
+            Assert.AreEqual(40, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Befuellen_ÜberKapazitaet()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            // Act
+            fass.Befuellen(150);
+            // Assert
+            Assert.AreEqual(100, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Befuellen_Negativ()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            // Act
+            fass.Befuellen(-100);
+            // Assert
+            Assert.AreEqual(0, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Entnehmen_Normal()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(80);
+            // Act
+            fass.Entnehmen(30);
+            // Assert
+            Assert.AreEqual(50, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Entnehmen_UnterMinimal()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(20);
+            // Act
+            fass.Entnehmen(50);
+            // Assert
+            Assert.AreEqual(0, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Entnehmen_Null()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(50);
+            // Act
+            fass.Entnehmen(0);
+            int nullEntleert = fass.Fuellstand;
+            // Assert
+            Assert.AreEqual(50, nullEntleert);
+        }
+        [TestMethod]
+        public void Entleeren_Test()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(60);
+            // Act
+            fass.Entleeren();
+            // Assert
+            Assert.AreEqual(0, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Entleeren_Test2()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(100);
+            // Act
+            fass.Entleeren();
+            // Assert
+            Assert.AreEqual(0, fass.Fuellstand);
+        }
+        [TestMethod]
+        public void Entleeren_LeeresFass()
+        {
+            // Arrange
+            Wasserfass fass = new Wasserfass(100);
+            fass.Befuellen(0);
+            // Act
+            fass.Entleeren();
+            // Assert
+            Assert.AreEqual(0, fass.Fuellstand);
         }
     }
 }
